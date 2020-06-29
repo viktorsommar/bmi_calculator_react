7c3f41dd7392523db1becb0f0433c5c5e81fb73c
viktor_sommar <vsommarbaillot@gmail.com>
2020-06-29 20:42:25 +0200

    Setting up BMI Calculator project.

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Form from "./components/Form";
+import Message from "./components/Message"
+import { calculateBmi } from "./helpers/bmiHelper";
+
+class App extends Component {
+  state = {
+    weight: "",
+    height: "",
+    bmiValue: "",
+    bmiMessage: ""
+  }
+
+  onChangeHandler = e => this.setState({ [e.target.name]: e.target.value })
+
+  onSubmitHandler = e => {
+    e.preventDefault()
+    const [bmiValue, bmiMessage] = calculateBmi(
+      this.state.weight,
+      this.state.height
+    )
+    this.setState({ bmiValue: bmiValue, bmiMessage: bmiMessage })
+  }
+
+  render() {
+    return (
+      <div>
+        <Form
+          weight={this.state.weight}
+          height={this.state.height}
+          onChangeHandler={this.onChangeHandler}
+          onSubmitHandler={this.onSubmitHandler}
+        />
+        {this.state.bmiValue && (
+          <Message
+            bmiValue={this.state.bmiValue}
+            bmiMessage={this.state.bmiMessage}
+          />
+        )}
+      </div>
+    )
+  }
+}
+
+export default App;
+
